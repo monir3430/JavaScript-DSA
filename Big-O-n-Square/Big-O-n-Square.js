f0bbf922c507d51a08b55a992fea828f075abcc9
monir3430 <miamonir3430@gmail.com>
2023-03-03 22:20:10 +0600

    Big O n Square

---
+++ Big-O-n-Square/Big-O-n-Square.js
@@ -0,0 +1,10 @@
+function nestedLoop(n){
+    for(let i=0; i<n; i++){
+        console.log(i);
+        for(let j=0; j<n; j++){
+            console.log(`Nested: ${j}`);
+        }
+    }
+}
+
+nestedLoop(10);
